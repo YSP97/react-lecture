632a03de46a7eafeb89961c9864b1d093266a5f5
YSP97 <pus9717@gmail.com>
2024-07-29 13:37:01 +0900

    [환경구성] Vite 커스텀 템플릿 - vite react 플러그인 설치

---
+++ 07-vite-custom-template/vite-custom-template/src/App.jsx
@@ -0,0 +1,11 @@
+import { jsx } from "react/jsx-runtime";
+
+console.log(jsx);
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>React 웹 앱</h1>
+    </div>
+  );
+}
