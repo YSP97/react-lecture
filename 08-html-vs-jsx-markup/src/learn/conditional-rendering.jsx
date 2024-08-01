9fa0f85a2917aa4f60558eef5df45caa8b63c13b
YSP97 <pus9717@gmail.com>
2024-08-01 10:06:50 +0900

    [강의] 배열을 사용한 리스트 렌더링 - revese VS toReversed

--- 08-html-vs-jsx-markup/src/learn/conditional-rendering.jsx
+++ 08-html-vs-jsx-markup/src/learn/conditional-rendering.jsx
@@ -1,10 +1,11 @@
-import { typeOf, randomNumber } from '../utils';
+import { isTrueOrFalse } from '../utils';
+import PropTypes from '../utils/prop-types';
 
 // 이미지 경로 불러오기
+import kakaoTalkImagePath from '../assets/kakao-talk.svg?url';
+import nextJsImagePath from '../assets/next-js.svg?url';
 import reactImagePath from '../assets/react.svg?url';
 import viteImagePath from '../assets/vite.svg?url';
-import nextJsImagePath from '../assets/next-js.svg?url';
-import kakaoTalkImagePath from '../assets/kakao-talk.svg?url';
 
 // 이미지 타입 배열 관리
 const IMAGE_TYPES = ['react', 'vite', 'next.js', 'kakao talk'];
@@ -17,38 +18,52 @@ function ConditionalRendering({ imageType }) {
   let imagePath = '';
   let printText = '';
 
-  if (imageType.toLowerCase().includes('react')) {
-    imagePath = reactImagePath;
-    printText = 'React';
+  // 조건 "문"
+  // if 문 => switch 문으로 변경
+  switch (imageType.toLowerCase()) {
+    case 'react':
+      imagePath = reactImagePath;
+      printText = 'React';
+      break;
+    case 'vite':
+      imagePath = viteImagePath;
+      printText = 'Vite';
+      break;
+    case 'next.js':
+      imagePath = nextJsImagePath;
+      printText = 'Next.js';
+      break;
+    case 'kakao talk':
+      imagePath = kakaoTalkImagePath;
+      printText = 'Kakao Talk';
+      break;
+    default:
+      printText = '허용된 이미지 타입이 존재하지 않습니다.';
   }
 
-  if (imageType.toLowerCase().includes('vite')) {
-    imagePath = viteImagePath;
-    printText = 'Vite';
-  }
+  // 값을 반환하는 함수 실행
+  // const isShowSpinner = isTrueOrFalse(); // 참 또는 거짓
 
-  if (imageType.toLowerCase().includes('next.js')) {
-    imagePath = nextJsImagePath;
-    printText = 'Next.js';
-  }
+  // 3항 연산자 (표현) "식"
+  const spinnerOrVite = isTrueOrFalse() ? (
+    <img className="spinner" src="/icons/spinner.svg" alt="로딩 중..." />
+  ) : (
+    <img src="/vite.svg" alt="Vite" style={{ height: 42 }} />
+  );
 
-  if (imageType.toLowerCase().includes('kakao talk')) {
-    imagePath = kakaoTalkImagePath;
-    printText = 'Kakao Talk';
-  }
-
-  // JSX 안에서는 문을 사용할 수 없다! 반드시 식만 쓸 수 있음(예를들어 삼항식 같은 건 가능한데 if문은 불가능)
-  const randomImage =
-    randomNumber(0, 1) > 0.5 ? (
-      <img className="spinner" src="/icons/spinner.svg" alt="로딩 중..." />
-    ) : (
-      <img src={viteImagePath} alt="Vite" style={{ height: 42 }} />
-    );
+  // 논리 곱/합 연산자
+  // 그리고(AND, &&), 또는(OR, ||)
+  // const spinnerMessage = isShowSpinner ? '스피너 표시' : null;
+  // const spinnerMessage = isShowSpinner && '스피너 표시';
+  // const spinnerMessage = !isShowSpinner || '스피너 표시';
 
   // JSX 반환 (마크업 생성)
+  // Q. JSX는 문(statement)이다? 아니, 표현식(expression)이다!
+  //    JSX 내부에서는 오직 식만 사용 가능하다!!!
   return (
     <>
-      <dt>조건부 렌더링(conditional rendering)</dt>
+      {/* <dt>조건부 렌더링(conditional rendering) ({spinnerMessage})</dt> */}
+      <dt>조건부 렌더링(conditional rendering) ({isTrueOrFalse() && '스피너 표시'})</dt>
       <dd>
         <p>이미지 타입(image type)에 따라 렌더링 여부를 결정합니다.</p>
         <div className="conditionalRendering">
@@ -60,7 +75,18 @@ function ConditionalRendering({ imageType }) {
       </dd>
       <dd style={{ marginTop: 12 }}>
         <p>spinner 또는 vite 이미지가 랜덤으로 화면에 렌더링 되도록 합니다.</p>
-        <div className="conditionalRendering">{randomImage}</div>
+        <div className="conditionalRendering">
+          {spinnerOrVite}
+          {/* {randomNumber(0, 1) > 0.5 ? (
+            <img
+              className="spinner"
+              src="/icons/spinner.svg"
+              alt="로딩 중..."
+            />
+          ) : (
+            <img src="/vite.svg" alt="Vite" style={{ height: 42 }} />
+          )} */}
+        </div>
       </dd>
     </>
   );
@@ -72,32 +98,8 @@ export default ConditionalRendering;
 // 컴포넌트 속성(들) 검사
 ConditionalRendering.propTypes = {
   // imageType 속성 검사
-  imageType(props, propName, componentName) {
-    // 컴포넌트 속성 값
-    const propValue = props[propName];
-
-    // 컴포넌트 속성 값의 타입
-    const propType = typeOf(propValue);
-
-    // 컴포넌트 속성이 허용하는 타입
-    const allowedType = 'string';
-
-    // 정규식을 사용해서 특정 문자 값이 매칭(일치)되는 지 검토
-    const typeMatchString = IMAGE_TYPES.reduce((result, type, index, array) => {
-      const divider = index < array.length - 1 ? '|' : '';
-      return result + type + divider;
-    }, '');
-
-    const typeMatch = new RegExp(`^(${typeMatchString})$`, 'i');
-
-    // 타입 검사 수행
-    if (propType !== allowedType || !typeMatch.test(propValue)) {
-      // 타입이 다르면? 오류 처리
-      throw new Error(
-        `${componentName} 컴포넌트 ${propName} 속성에 설정 가능한 값은 "[${IMAGE_TYPES.toString()}]" 중 하나가 요구되나, 실제 전달된 속성 값은 "${propValue}"입니다.`
-      );
-    }
-
-    // 타입이 동일하고 속성 값이 매칭(일치)되면? 통과
-  },
+  // 배열에 포함된 것 중 하나를 확인하는 검사
+  // ['react', 'vite', 'next.js', 'kakao talk']
+  // imageType: oneOf(['react', 'vite', 'next.js', 'kakao talk']),
+  imageType: PropTypes.oneOf(IMAGE_TYPES),
 };
--- 08-html-vs-jsx-markup/src/learn/render-lists.jsx
+++ 08-html-vs-jsx-markup/src/learn/render-lists.jsx
@@ -1,28 +1,61 @@
-function RenderLists() {
-  const renderList = () => {};
+import { array } from '../utils/prop-types';
+
+function RenderLists({ items /* string[], Array<string> */ }) {
+  // 함수 내부에 리스트 렌더링 코드를 작성해보세요.
+  const renderList = ({ reverse = false } = {}) => {
+    // 리스트 렌더링 결과 반환
+    // - [ ] Array.prototype.forEach? // 반환값이 없기때문에 forEach는 X
+    // - [x] Array.prototype.map?
+
+    // reverse: true인 경우 역순 정렬
+    let listItems = items;
+    if (reverse) {
+      listItems = items.toReversed();
+      // 원본배열을 훼손하지 않기때문에 reverse보다 toReversed(E5 2023 v14 추가됨)를 사용하는 것이 좋다!
+    }
+
+    return listItems.map((item) /* string */ => {
+      // console.log(item);
+      // JSX(React Element) Markup
+      return <li key={item}>{item}</li>;
+    });
+  };
 
   return (
     <>
       <dt>리스트 렌더링(list rendering)</dt>
       <dd>
-        <p>상태 메시지(status message) 배열을 리스트 렌더링합니다.</p>
+        <p>상태 메시지(status messages) 배열을 리스트 렌더링합니다.</p>
         <ul className="renderList">{renderList?.()}</ul>
       </dd>
       <dd>
-        <p>상태 메시지(status message) 배열을 역순 정렬하여 렌더링합니다.</p>
+        <p>상태 메시지(status messages) 배열을 역순 정렬하여 렌더링합니다.</p>
         <ul className="renderList">{renderList?.({ reverse: true })}</ul>
       </dd>
       <dd>
         <p>
-          React 라이브러리(reactLibrary) 객체의 키, 값을 <q>설명 목록</q>으로
-          렌더링합니다.
+          React 라이브러리(reactLibrary) 객체의 키, 값을 <q>설명 목록</q>으로 렌더링합니다.
         </p>
-        <dl className="reactLibrary">
-          {/* 여기서 설명 목록으로 리스트 렌더링 합니다. */}
-        </dl>
+        <dl className="reactLibrary">{/* 여기서 설명 목록으로 리스트 렌더링 합니다. */}</dl>
       </dd>
     </>
   );
 }
 
 export default RenderLists;
+
+RenderLists.propTypes = {
+  // D.R.Y
+  // Reusability
+  // Mechanism
+  // - [x] Function
+  // - [ ] Class
+  // NEEDS
+  // Library (Utilities)
+  // items(props, propName, componentName) {
+  //   const propValue = props[propName];
+  //   const propType = typeOf(propValue);
+
+  // }
+  items: array,
+};
