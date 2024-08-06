65e958eed1a2c091175a3c9adbbade95f081fc4d
YSP97 <pus9717@gmail.com>
2024-08-06 15:38:20 +0900

    [강의] 리액트 상태 업데이트 - 다른 컴포넌트와 상태 공유하기 - 상태 끌어올리기 전략 - props를 전달하는 방식

--- 12-react-component-states/src/pages/users/components/UserSearchBox.jsx
+++ 12-react-component-states/src/pages/users/components/UserSearchBox.jsx
@@ -1,20 +1,33 @@
-import { string } from 'prop-types';
+import { func, string } from 'prop-types';
 import './UserSearchBox.css';
 import { useId, useState } from 'react';
 
-function UserSearchBox() {
+UserSearchBox.propTypes = {
+  searchTerm: string.isRequired,
+  onSearch: func,
+};
+
+function UserSearchBox({ searchTerm, onSearch }) {
   // useId: 리액트에서 제공하는 접근성을 위한 id 어트리뷰트 생성하는 hook
   const id = useId();
 
-  const [searchTerm, setSearchTerm] = useState('');
-  const handleChange = (e) => {
-    setSearchTerm(e.target.value);
-    console.log({ searchTerm });
-  };
+  // const handleChange = (e) => {
+  //   setSearchTerm(e.target.value);
+  // };
 
   const handleSearch = () => {
-    console.log(searchTerm);
+    console.log('search');
+    // sideEffectCode
+    // DOM 접근, 속성 값 읽기
+    const input = document.getElementById(id);
+    const value = input.value.trim();
+
+    if (value.length > 0) {
+      onSearch?.(value);
+    }
+    // onSearch?.();
   };
+
   return (
     <div className="UserSearchBox">
       <div className="control">
@@ -24,7 +37,12 @@ function UserSearchBox() {
           type="search"
           id={id}
           placeholder="사용자 이름 입력"
-          onChange={handleChange}
+          // value={searchTerm}
+          // onchange 사용하지 않으면 value값만 있는 읽기전용임
+          // 읽기전용만 사용하려면 defaultValue로 설정할 것
+          //  defaultValue는 렌더링 시점에만 해당 input의 value를 채우고 이후에는 제어하지 않는다
+          // onChange={handleChange}
+          defaultValue={searchTerm}
         />
       </div>
       <button type="button" onClick={handleSearch}>
