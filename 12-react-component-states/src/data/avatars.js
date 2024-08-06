cf48927baa0f3fc993b446834a37453617b89e79
YSP97 <pus9717@gmail.com>
2024-08-06 14:48:49 +0900

    [강의] 리액트 상태 업데이트 - 접근성 사용성 고려 - useID() 훅 함수, htmlFor 속성 사용 이유 - search 타입 입력 필드 사용 이유

--- 12-react-component-states/src/data/avatars.js
+++ 12-react-component-states/src/data/avatars.js
@@ -1,28 +1,28 @@
 import { STATUS } from '@/constants/STATUS';
-const avatarsData = [
+
+export const avatarsData = [
   {
     id: 'avatar-abc',
     name: '야무',
-    photo: 'man-02.jpg',
+    photo: 'man-01.jpg',
     status: STATUS.online,
   },
   {
     id: 'avatar-def',
     name: '범쌤',
-    photo: 'man-04.jpg',
-    status: STATUS.away,
+    photo: 'man-02.jpg',
+    status: STATUS.dontDisturb,
   },
   {
     id: 'avatar-ghi',
     name: '주원',
-    photo: 'woman-04.jpg',
-    status: STATUS.dontDisturb,
+    photo: 'woman-01.jpg',
+    status: STATUS.offline,
   },
   {
     id: 'avatar-jkl',
     name: '정민',
-    photo: 'woman-01.jpg',
+    photo: 'woman-02.jpg',
+    status: STATUS.away,
   },
 ];
-
-export default avatarsData;
--- 12-react-component-states/src/pages/users/components/UserSearchBox.jsx
+++ 12-react-component-states/src/pages/users/components/UserSearchBox.jsx
@@ -1,9 +1,35 @@
+import { string } from 'prop-types';
+import './UserSearchBox.css';
+import { useId, useState } from 'react';
+
 function UserSearchBox() {
+  // useId: 리액트에서 제공하는 접근성을 위한 id 어트리뷰트 생성하는 hook
+  const id = useId();
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+    console.log({ searchTerm });
+  };
+
+  const handleSearch = () => {
+    console.log(searchTerm);
+  };
   return (
     <div className="UserSearchBox">
-      <label>사용자 검색</label>
-      <input type="search" placeholder="사용자 이름 입력" />
-      <button type="button">찾기</button>
+      <div className="control">
+        <label htmlFor={id}>사용자 검색</label>
+        {/* useRef */}
+        <input
+          type="search"
+          id={id}
+          placeholder="사용자 이름 입력"
+          onChange={handleChange}
+        />
+      </div>
+      <button type="button" onClick={handleSearch}>
+        찾기
+      </button>
     </div>
   );
 }
