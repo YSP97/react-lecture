b47bf634e7e19e019b42e68806c1f1643af4273d
YSP97 <pus9717@gmail.com>
2024-08-08 09:22:20 +0900

    [강의] 추가 기능 구현 - 강의 세팅 - enter키 입력 시 찾기 - focus

--- 12-react-component-states/src/pages/users/components/UserSearchBox.jsx
+++ 12-react-component-states/src/pages/users/components/UserSearchBox.jsx
@@ -1,54 +1,53 @@
-import { func, string } from 'prop-types';
+import { useId } from 'react';
+import { string, func } from 'prop-types';
 import './UserSearchBox.css';
-import { useId, useState } from 'react';
 
 UserSearchBox.propTypes = {
   searchTerm: string.isRequired,
-  onSearch: func,
+  onSearch: func, // optional
 };
 
 function UserSearchBox({ searchTerm, onSearch }) {
-  // useId: 리액트에서 제공하는 접근성을 위한 id 어트리뷰트 생성하는 hook
   const id = useId();
 
-  // const handleChange = (e) => {
-  //   setSearchTerm(e.target.value);
-  // };
-
-  const handleSearch = () => {
-    console.log('search');
-    // sideEffectCode
+  const handleSearch = (e) => {
+    e.preventDefault();
+    // Side Effects
     // DOM 접근, 속성 값 읽기
     const input = document.getElementById(id);
     const value = input.value.trim();
+    const button = input.closest('form').querySelector('[type="submit"]');
+    console.log(button);
 
     if (value.length > 0) {
       onSearch?.(value);
+      input.value = '';
+      button.focus();
+    } else {
+      alert('검색어를 입력해주세요.');
+      input.focus();
+      input.value = '';
     }
-    // onSearch?.();
   };
 
   return (
-    <div className="UserSearchBox">
+    <form className="UserSearchBox">
       <div className="control">
         <label htmlFor={id}>사용자 검색</label>
-        {/* useRef */}
         <input
-          type="search"
           id={id}
-          placeholder="사용자 이름 입력"
-          // value={searchTerm}
-          // onchange 사용하지 않으면 value값만 있는 읽기전용임
-          // 읽기전용만 사용하려면 defaultValue로 설정할 것
-          //  defaultValue는 렌더링 시점에만 해당 input의 value를 채우고 이후에는 제어하지 않는다
-          // onChange={handleChange}
+          type="search"
+          placeholder="사용자 정보 입력"
           defaultValue={searchTerm}
+          // value={searchTerm}
+          // onChange={handleChange}
+          // readOnly
         />
       </div>
-      <button type="button" onClick={handleSearch}>
+      <button type="submit" onClick={handleSearch}>
         찾기
       </button>
-    </div>
+    </form>
   );
 }
 
