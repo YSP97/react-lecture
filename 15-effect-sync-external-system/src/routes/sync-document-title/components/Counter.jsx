96d7beda964d20ea94c4da410898132b0916c7d8
YSP97 <pus9717@gmail.com>
2024-08-14 14:43:47 +0900

    [강의] effect 활용 - 리액트 외부 시스템과 동기화 - 브라우저의 문서 제목 동기화 - 이펙트 추가 -> useEffect 훅 사용 -이펙트 콜백 함수 설정 - 종속성, 의존성 설정(반응성 데이터를 배열에 추가)

--- 15-effect-sync-external-system/src/routes/sync-document-title/components/Counter.jsx
+++ 15-effect-sync-external-system/src/routes/sync-document-title/components/Counter.jsx
@@ -1,30 +1,61 @@
 // --------------------------------------------------------------------------
 // ✅ 문서 제목 동기화
 // --------------------------------------------------------------------------
+// - [ ] 이펙트 작성(추가)하는 방법!
 // - [ ] 카운터 앱의 count 상태가 변경되면 문서 제목을 동기화합니다.
 // - [ ] step 값이 변경될 때에는 불필요한 문서 제목 동기화가 되지 않도록 설정합니다.
 // --------------------------------------------------------------------------
 
-import { useId, useState } from 'react';
+import { useId, useState, useEffect } from 'react';
 import S from './Counter.module.css';
 
+// 문서의 초기제목
+const DOCUMENT_TITLE = document.title;
+
 function Counter() {
+  // 컴포넌트 JSX가 실제 DOM 요소로 마운트 되었니?
+  // [이펙트] useEffect
+  // 첫번째 인수: effect callback 함수 설정
+  // - refCallback은 마운트시 한 번만 실행됨
+  // - useEffect는 마운트 된 이후 실행되는데 값이 변할때마다 실행됨
+  // - 상태가 변경될때마다 실행됨
+
+  // 두번째 인수: 종속성(의존성) 배열
+  // - 설정하지 않은 경우, 렌더링 시마다 매번 실행
+  // - 배열이 비어있는 경우, 마운트 시점에서 1회 실행됨 (strict mode에서는 2번 시행)
+
   const id = useId();
-  const [count, setCount] = useState(0);
-  const [step, setStep] = useState(1);
+
+  const [state, setState] = useState({
+    count: 0,
+    step: 1,
+  });
+
+  const { count, step } = state;
+
+  // 이런식으로 관심사끼리 묶어두는 것이 좋다!
+  // const [count, setCount] = useState(1);
+  useEffect(() => {
+    document.title = `(${count})` + DOCUMENT_TITLE;
+  }, [count]);
+
+  // const [step, setStep] = useState(1);
+  useEffect(() => {
+    console.log(`${step}이 변경됨`);
+  }, [step]);
 
   const handleDecrease = () => {
     let nextCount = count - step;
     if (nextCount <= 1) nextCount = 1;
-    setCount(nextCount);
+    setState({ ...state, count: nextCount });
   };
 
   const handleIncrease = () => {
-    setCount(count + step);
+    setState({ ...state, count: count + step });
   };
 
   const handleChangeStep = (e) => {
-    setStep(Number(e.target.value));
+    setState({ ...state, step: Number(e.target.value) });
   };
 
   const isDisabled = count <= 1;
